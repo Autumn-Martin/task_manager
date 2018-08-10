1d7c3f3d5460798447961425ace4c8c82f7b1454
Autumn-Martin <autumnmartin.dev@gmail.com>
2018-08-10 15:16:00 -0600

    add method to write to database

--- app/models/task.rb
+++ app/models/task.rb
@@ -4,6 +4,10 @@ class Task
     @description = task_params["description"]
     @title       = task_param["title"]
     @database = SQLite3::Database.new('db/task_manager_development.db')
-    @database.results_as_hash = true
+    @database.results_as_hash = true # overrides default of returning rows as arrays
+  end
+
+  def save
+    @database.execute("INSERT INTO tasks (title, description) VALUES (?, ?);", @title, @description)
   end
 end
