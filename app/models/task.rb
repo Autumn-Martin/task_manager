a802b2752815bd0126e8fb5968148a552d734330
Autumn-Martin <autumnmartin.dev@gmail.com>
2018-08-10 15:34:43 -0600

    resolve bug

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,7 +2,7 @@ require 'sqlite3'
 class Task
   def initialize(task_params)
     @description = task_params["description"]
-    @title       = task_param["title"]
+    @title       = task_params["title"]
     @database = SQLite3::Database.new('db/task_manager_development.db')
     @database.results_as_hash = true # overrides default of returning rows as arrays
   end
