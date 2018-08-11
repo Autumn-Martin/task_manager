96418077e3598ce548b59fea23c4eff88c6b5fd7
Autumn-Martin <autumnmartin.dev@gmail.com>
2018-08-11 13:15:36 -0600

    resolving errors

--- app/controllers/task_manager_app.rb
+++ app/controllers/task_manager_app.rb
@@ -2,7 +2,7 @@ require_relative "../models/task.rb"
 
 class TaskManagerApp < Sinatra::Base
   set :root, File.expand_path("..", __dir__)
-  set :method_override, true
+  # set :method_override, true
 
   get '/' do
     erb :dashboard #look for an erb file called  'dashboard'
@@ -33,13 +33,13 @@ class TaskManagerApp < Sinatra::Base
     erb :edit
   end
 
-  put '/tasks/:id' do |id|
-    Task.update(id.to_i, params[:task])
-    redirect "/tasks/#{id}"
-  end
-
-  delete '/tasks:id' do |id|
-    Task.destroy(id.to_i)
-    redirect '/tasks'
-  end
+  # put '/tasks/:id' do |id|
+  #   Task.update(id.to_i, params[:task])
+  #   redirect "/tasks/#{id}"
+  # end
+  #
+  # delete '/tasks:id' do |id|
+  #   Task.destroy(id.to_i)
+  #   redirect '/tasks'
+  # end
 end
